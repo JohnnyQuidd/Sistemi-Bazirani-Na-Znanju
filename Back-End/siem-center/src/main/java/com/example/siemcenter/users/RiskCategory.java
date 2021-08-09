1b83cc930d841d1d81303f55a0d18e707eedfaad
Petar Kovacevic <petar.kovacevic0088@gmail.com>
2021-08-09 19:16:43 +0200

    User model

---
+++ Back-End/siem-center/src/main/java/com/example/siemcenter/users/RiskCategory.java
@@ -0,0 +1,5 @@
+package com.example.siemcenter.users;
+
+public enum RiskCategory {
+    LOW, MODERATE, HIGH, EXTREME
+}
---
+++ Back-End/siem-center/src/main/java/com/example/siemcenter/users/Role.java
@@ -0,0 +1,5 @@
+package com.example.siemcenter.users;
+
+public enum Role {
+    USER, ADMIN, OPERATOR
+}
