043a0b8e9073c27aacb32fdbf8dfe3a3ce95b526
Petar Kovacevic <petar.kovacevic0088@gmail.com>
2021-08-09 16:40:20 +0200

    Back-End Spring Application intialized

---
+++ Back-End/siem-center/src/main/java/com/example/siemcenter/SiemCenterApplication.java
@@ -0,0 +1,13 @@
+package com.example.siemcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiemCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiemCenterApplication.class, args);
+	}
+
+}
---
+++ Back-End/siem-center/src/test/java/com/example/siemcenter/SiemCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.siemcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiemCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
